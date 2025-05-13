5feece14b335df0973d714d3aad802737439b089
Temayoo <Matt.yo95280@gmail.com>
2025-05-13 10:34:43 +0200

    Add Model for Transaction we use Type for Income or Expense

--- src/main/java/fr/coding/sfq/models/Transaction.java
+++ src/main/java/fr/coding/sfq/models/Transaction.java
@@ -1,4 +1,38 @@
 package fr.coding.sfq.models;
 
+import java.time.LocalDateTime;
+
 public class Transaction {
+
+    public enum Type {
+        INCOME, EXPENSE
+    }
+
+    private final LocalDateTime timestamp;
+    private final Type type;
+    private final double amount;
+    private final String description;
+
+    public Transaction(LocalDateTime timestamp, Type type, double amount, String description) {
+        this.timestamp = timestamp;
+        this.type = type;
+        this.amount = amount;
+        this.description = description;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }

391ba132f32493046d7098361e737a9467f9c020
Elwan <144356501+ElwanBeauchot@users.noreply.github.com>
2025-05-13 13:32:13 +0200

    Employee Controller without db

---
+++ src/main/java/fr/coding/sfq/EmployeeController.java
@@ -0,0 +1,123 @@
+package fr.coding.sfq;
+
+import fr.coding.sfq.models.Employee;
+import javafx.scene.control.*;
+import javafx.stage.StageStyle;
+import javafx.util.Callback;
+import javafx.scene.layout.HBox;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+
+public class EmployeeController {
+
+    @FXML private TableView<Employee> employeeTable;
+    @FXML private TableColumn<Employee, String> employeeNameColumn;
+    @FXML private TableColumn<Employee, String> employeePositionColumn;
+    @FXML private TableColumn<Employee, Number> employeeWorkHoursColumn;
+    @FXML private TableColumn<Employee, Void> actionsColumn;
+
+    @FXML private TextField employeeNameField;
+    @FXML private TextField employeePositionField;
+    @FXML private Button createEmployeeButton;
+
+    @FXML private Button homeButton;
+
+    private ObservableList<Employee> employees = FXCollections.observableArrayList();
+
+    @FXML
+    public void initialize() {
+        employeeNameColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getName()));
+        employeePositionColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getPosition()));
+        employeeWorkHoursColumn.setCellValueFactory(cellData -> new SimpleDoubleProperty(cellData.getValue().getWorkHours()));
+        employeeTable.setItems(employees);
+
+        createEmployeeButton.setOnAction(event -> createTable());
+
+        homeButton.setOnAction(event -> MainController.getInstance().switchView("HomePage.fxml"));
+
+        addActionsToTable();
+
+        employeeTable.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
+    }
+
+
+    private void createTable() {
+        String employeeName;
+        String employeePostion;
+
+        try {
+            employeeName = employeeNameField.getText();
+            employeePostion = employeePositionField.getText();
+        } catch (NumberFormatException e) {
+            System.out.println("Nom ou poste de l'employé invalide");
+            return;
+        }
+
+        Employee newEmployee = new Employee(employeeName, 0, employeePostion);
+        employees.add(newEmployee);
+        employeeTable.refresh(); // Refresh TableView to show new tables
+
+        employeeNameField.clear();
+        employeePositionField.clear();
+    }
+
+    private void addActionsToTable() {
+        Callback<TableColumn<Employee, Void>, TableCell<Employee, Void>> cellFactory = new Callback<>() {
+            @Override
+            public TableCell<Employee, Void> call(final TableColumn<Employee, Void> param) {
+                return new TableCell<>() {
+
+                    private final Button addHoursBtn = new Button("Ajouter Heures Travaillées");
+                    private final Button deleteBtn = new Button("Virer l'employé");
+                    private final HBox pane = new HBox(5, addHoursBtn, deleteBtn);
+
+                    {
+                        addHoursBtn.setOnAction(event -> {
+                            Employee employee = getTableView().getItems().get(getIndex());
+
+                            TextInputDialog dialog = new TextInputDialog();
+                            dialog.setTitle("Ajouter des heures");
+                            dialog.setHeaderText("Ajouter des heures pour : " + employee.getName());
+                            dialog.setContentText("Heures à ajouter :");
+
+                            dialog.showAndWait().ifPresent(input -> {
+                                try {
+                                    double additionalHours = Double.parseDouble(input);
+                                    employee.setWorkHours(employee.getWorkHours() + additionalHours);
+                                    employeeTable.refresh();
+                                } catch (NumberFormatException e) {
+                                    System.out.println("Entrée invalide : doit être un nombre.");
+                                }
+                            });
+                        });
+
+                        deleteBtn.setOnAction(event -> {
+                            Employee employee = getTableView().getItems().get(getIndex());
+                            employees.remove(employee);
+                            employeeTable.refresh();
+                        });
+                    }
+
+                    @Override
+                    protected void updateItem(Void item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (empty) {
+                            setGraphic(null);
+                        } else {
+                            setGraphic(pane);
+                        }
+                    }
+                };
+            }
+        };
+
+        actionsColumn.setCellFactory(cellFactory);
+    }
+
+
+}
--- src/main/java/fr/coding/sfq/HomeController.java
+++ src/main/java/fr/coding/sfq/HomeController.java
@@ -7,10 +7,12 @@ public class HomeController {
 
     @FXML private Button goToDishesButton;
     @FXML private Button tablesButton;
+    @FXML private Button employeeButton;
 
     @FXML
     public void initialize() {
         goToDishesButton.setOnAction(event -> MainController.getInstance().switchView("DishView.fxml"));
         tablesButton.setOnAction(event -> MainController.getInstance().switchView("TableView.fxml"));
+        employeeButton.setOnAction(event -> MainController.getInstance().switchView("EmployeeView.fxml"));
     }
 }

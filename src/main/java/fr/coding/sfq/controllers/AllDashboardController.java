492c2a749526f9e948314120dc763c229097ad97
unknown <dupontstef80@gmail.com>
2025-05-14 21:38:05 +0200

    style(dashboard): design table available

--- src/main/java/fr/coding/sfq/controllers/AllDashboardController.java
+++ src/main/java/fr/coding/sfq/controllers/AllDashboardController.java
@@ -17,7 +17,10 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 import org.hibernate.Session;
 
@@ -35,10 +38,14 @@ public class AllDashboardController {
 
     @FXML private VBox mine;
 
+    ObservableList<TablesEntity> orders = FXCollections.observableArrayList();
+    ObservableList<TablesEntity> tables = FXCollections.observableArrayList();
+
     public void initialize() {
         Platform.runLater(() -> {
             try(Session session = HibernateUtil.getSessionFactory().openSession()){
                 orders = FXCollections.observableArrayList(session.createQuery("FROM TablesEntity o JOIN OrdersEntity t ON t.id = o.order.id", TablesEntity.class).stream().collect(Collectors.toList()));
+                tables = FXCollections.observableArrayList(session.createQuery("FROM TablesEntity ", TablesEntity.class).stream().collect(Collectors.toList()));
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -50,7 +57,6 @@ public class AllDashboardController {
 
     }
 
-    ObservableList<TablesEntity> orders = FXCollections.observableArrayList();
     private void loadOrders() {
         TableNo.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getLocation()));
 
@@ -63,10 +69,28 @@ public class AllDashboardController {
     private void loadTableStatuses() {
         // Fetch table statuses and update tableStatusGrid
         System.out.println(orders.size());
-        orders.stream().map(TablesEntity::getLocation).forEach(fn -> {
-            mine.getChildren().add(new Label(fn));
+        tables.stream().filter(TablesEntity::isOccupied).map(TablesEntity::getLocation).forEach(fn -> {
+            HBox box = new HBox();
+
+            box.getChildren().add(new Label(fn));
+
+            box.getChildren().add(new Circle(10, Color.GREEN));
+
+            mine.getChildren().add(box);
 //                tableGrid.add(new Text("test"), 1, 1);
             }
+        );
+
+        tables.stream().filter(fn -> !fn.isOccupied()).map(TablesEntity::getLocation).forEach(fn -> {
+                    HBox box = new HBox();
+
+                    box.getChildren().add(new Label(fn));
+
+                    box.getChildren().add(new Circle(10, Color.RED));
+
+                    mine.getChildren().add(box);
+//                tableGrid.add(new Text("test"), 1, 1);
+                }
         );
     }
 }

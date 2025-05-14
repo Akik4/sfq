0d2c8a11e561d52f2b78a13346c16717dd5dfdf2
unknown <dupontstef80@gmail.com>
2025-05-14 16:14:50 +0200

    feat(dashboard): creating page

---
+++ src/main/java/fr/coding/sfq/controllers/AllDashboardController.java
@@ -0,0 +1,72 @@
+package fr.coding.sfq.controllers;
+
+import fr.coding.sfq.models.OrdersEntity;
+import fr.coding.sfq.models.TablesEntity;
+import fr.coding.sfq.util.HibernateUtil;
+import jakarta.persistence.Column;
+import javafx.application.Platform;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import org.hibernate.Session;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class AllDashboardController {
+
+    @FXML private TableColumn<TablesEntity, Integer> OrderID;
+    @FXML private TableColumn<TablesEntity, String> TableNo;
+    @FXML private TableColumn<TablesEntity, Boolean> Status;
+
+    @FXML private TableView<TablesEntity> ordersTable;
+    @FXML private GridPane tableGrid;
+
+    @FXML private VBox mine;
+
+    public void initialize() {
+        Platform.runLater(() -> {
+            try(Session session = HibernateUtil.getSessionFactory().openSession()){
+                orders = FXCollections.observableArrayList(session.createQuery("FROM TablesEntity o JOIN OrdersEntity t ON t.id = o.order.id", TablesEntity.class).stream().collect(Collectors.toList()));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            loadOrders();
+            loadTableStatuses();
+        });
+
+
+    }
+
+    ObservableList<TablesEntity> orders = FXCollections.observableArrayList();
+    private void loadOrders() {
+        TableNo.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getLocation()));
+
+        OrderID.setCellValueFactory(cellData -> new SimpleIntegerProperty(cellData.getValue().getOrder().getId()).asObject());
+                Status.setCellValueFactory(cellData -> new SimpleBooleanProperty(cellData.getValue().isOccupied()));
+
+                ordersTable.setItems(orders);
+    }
+
+    private void loadTableStatuses() {
+        // Fetch table statuses and update tableStatusGrid
+        System.out.println(orders.size());
+        orders.stream().map(TablesEntity::getLocation).forEach(fn -> {
+            mine.getChildren().add(new Label(fn));
+//                tableGrid.add(new Text("test"), 1, 1);
+            }
+        );
+    }
+}
--- src/main/java/fr/coding/sfq/controllers/HomeController.java
+++ src/main/java/fr/coding/sfq/controllers/HomeController.java
@@ -1,10 +1,14 @@
 package fr.coding.sfq.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class HomeController {
 
+    @FXML private Button dashboardButton;
+
     @FXML
     public void initialize() {
-   }
+        dashboardButton.setOnAction(event -> {MainController.getInstance().switchView("AllDashboardView.fxml");});
+    }
 }
